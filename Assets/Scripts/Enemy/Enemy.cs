c15dabdc2a86ee7905b2ca11682107f3d333f209
David <daviduzno@protonmail.com>
2024-10-29 17:55:34 -0300

    refactor: Crear clase base 'Character' para manejar salud y daño
    - Se creó la clase 'Character' que contiene la lógica común para el manejo de salud y daño.
    - Las clases 'Player' y 'Enemy' ahora heredan de 'Character', eliminando código duplicado.
    
    Co-Authored-By: MateoMonchi <145404747+MateoMonchi@users.noreply.github.com>

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,22 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
-    [SerializeField] private int _health = 100;
 
-    public void TakeDamage(int damage)
-    {
-        _health -= damage;
-
-        if (_health <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        Destroy(gameObject); 
-    }
 }
