a54287288bba8ee3760c2663d924dbd0b6d4d54c
David <141585041+David-Uzno@users.noreply.github.com>
2024-11-05 17:42:50 -0300

    feat: WorldMap Funcional con Clicks

--- Assets/Scripts/World Map/Overworld.cs
+++ Assets/Scripts/World Map/Overworld.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class Overworld : MonoBehaviour
 {
+    [SerializeField] private int _numerLevel;
+    [SerializeField] private List<SceneAsset> _levels;
 
+    void OnMouseDown()
+    {
+        if (_numerLevel >= 0 && _numerLevel < _levels.Count)
+        {
+            SceneManager.LoadScene(_levels[_numerLevel].name);
+        }
+        else
+        {
+            Debug.LogError("Número de nivel inválido");
+        }
+    }
 }
