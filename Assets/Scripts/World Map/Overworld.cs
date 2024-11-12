f4e92b872bc3a2b613ecff54207074f9fd496a58
David-Uzno <daviduzno@protonmail.com>
2024-11-12 20:35:04 -0300

    feat: Botón Play de la UI de Selector del Power-Ups

--- Assets/Scripts/World Map/Overworld.cs
+++ Assets/Scripts/World Map/Overworld.cs
@@ -9,15 +9,23 @@ public class Overworld : MonoBehaviour
     [SerializeField] private int _numerLevel;
     [SerializeField] private List<SceneAsset> _levels;
 
+    [Header("UI")]
+    [SerializeField] private GameObject _powerUpsUI;
+
     void OnMouseDown()
     {
         if (_numerLevel >= 0 && _numerLevel < _levels.Count)
         {
-            SceneManager.LoadScene(_levels[_numerLevel].name);
+            _powerUpsUI.SetActive(true);
         }
         else
         {
             Debug.LogError("Número de nivel inválido");
         }
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(_levels[_numerLevel].name);
     }
 }
