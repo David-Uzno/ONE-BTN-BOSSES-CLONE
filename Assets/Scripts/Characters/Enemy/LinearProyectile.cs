1dbc65b2435ad4138d91daa42ed0f49af34c9246
MateoMonchi <monchiettimateo1@gmail.com>
2024-11-16 17:22:48 -0300

    Feat:ahoraSiCasiListoElDIsparoRecto

--- Assets/Scripts/Characters/Enemy/LinearProyectile.cs
+++ Assets/Scripts/Characters/Enemy/LinearProyectile.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class LinearProyectile : Projectile
+public class LinearProjectile : Projectile
 {
+    [Header("Projectile Lifetime")]
+    [SerializeField] private float _lifetime = 5f; 
+
     private void Start()
     {
         _speed = 0;
+
+        Destroy(gameObject, _lifetime);
     }
 
     protected override void HandleCollision(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-           
             Destroy(other.gameObject);
-          
+            Destroy(gameObject);
         }
     }
 }
+
 
