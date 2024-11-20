93246bb7015de705f3e011e7ec93c27efe73ea67
David <141585041+David-Uzno@users.noreply.github.com>
2024-11-19 23:55:59 -0300

    feat: Dash Inicial

--- Assets/Scripts/Characters/Player/Movements/DashPlayer.cs
+++ Assets/Scripts/Characters/Player/Movements/DashPlayer.cs
@@ -4,7 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DashPlayer : Character
+public class DashPlayer : PlayerMovement
 {
-    
+    [Header("Dash")]
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private int _dashCounter = 100;
+
+    protected override void Movement()
+    {
+        _collider.enabled = false;
+    }
+
+    protected override void StopMovement()
+    {
+        _collider.enabled = true;
+    }
 }
